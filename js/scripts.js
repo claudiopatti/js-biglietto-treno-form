e6bf1b3494b071496ebe4b5f4bde196555a96312
claudiopatti <claudiopatti1997@gmail.com>
2024-07-15 00:57:43 +0200

    aggiunto input nome e aggiunto biglietto con i dati dopo che si preme bottone

--- js/scripts.js
+++ js/scripts.js
@@ -53,10 +53,10 @@ console.log('ciao console')
 // //     // console.log(kilometers.value, age.value);
 // //     kilometers = (kilometers.value)
 // //     age = (age.value)
-    
+
 
 // //     console.log('chilometri da percorrere:' + kilometers);
-    
+
 // //     console.log('Età:' + age);
 
 // //     const priceTicket = (Math.round((kilometers * 0.21) * 100) / 100);
@@ -72,9 +72,9 @@ console.log('ciao console')
 // //     const ageMinor = 17;
 // //     const ageOver = 65;
 
-    
+
 // //     if (age <= ageMinor) {
-        
+
 // //         document.getElementById('resultPrice').innerHTML = priceTicketForMinor + '€'
 // //         console.log('prezzo scontato per minori:' + priceTicketForMinor + '€');
 // //     }
@@ -88,23 +88,32 @@ console.log('ciao console')
 // //         console.log('prezzo normale:' + priceTicket + '€');
 
 // //     }
-    
+
 
 // // }
 
 const infoForm = document.querySelector('form');
 
 infoForm.addEventListener('submit',
-    function(event) {
+    function (event) {
 
         event.preventDefault();
-    
+
 
         const inputKilometers = document.getElementById('kilometers');
-        console.log('inputKilometers.value', inputKilometers.value );
-        
+        console.log('inputKilometers.value', inputKilometers.value);
+
         const inputAge = document.getElementById('age');
-        console.log('inputAge.value', inputAge.value );
+        console.log('inputAge.value', inputAge.value);
+
+        const inputName = document.getElementById('name');
+        document.querySelector('.resultName').innerHTML = inputName.value;
+        console.log('inputName.value', inputName.value);
+
+        document.getElementById("numberSet").innerHTML = 'Carrozza' + "   " + Math.floor(Math.random() * 16);
+        console.log('numberSet' , numberSet )
+
+        document.getElementById("numberTicket").innerHTML = Math.floor(Math.random() * 1000001);
 
 
 
@@ -118,26 +127,43 @@ infoForm.addEventListener('submit',
         // const priceTicketForOver = inputKilometers.value * (0.21 * (1 - 0.4));
         // console.log('prezzo del biglietto per anziani:' + priceTicketForOver.toFixed(2) + '€');
 
-        
-        
+
+
         if (inputAge.value <= 17) {
             const priceTicketForMinor = (Math.round((inputKilometers.value * (0.21 * (1 - 0.2))) * 100) / 100);
-            document.getElementById('resultPrice').innerHTML = 'scontato per minori:' + priceTicketForMinor + '€'
+            document.querySelector('.resultPrice').innerHTML = "MAMMA E PAPA':" + priceTicketForMinor + '€'
             console.log('prezzo scontato per minori:' + priceTicketForMinor + '€');
         }
         else if (inputAge.value >= 65) {
             const priceTicketForOver = inputKilometers.value * (0.21 * (1 - 0.4));
-            document.getElementById('resultPrice').innerHTML ='scontato per anziani:' + priceTicketForOver.toFixed(2) + '€'
+            document.querySelector('.resultPrice').innerHTML = 'CO LI SORDI DELLA PENSIONE:' + priceTicketForOver.toFixed(2) + '€'
             console.log('prezzo scontato per anziani:' + priceTicketForOver.toFixed(2) + '€');
         }
 
         else {
             const priceTicket = (Math.round((inputKilometers.value * 0.21) * 100) / 100);
-            document.getElementById('resultPrice').innerHTML = 'normale:' + priceTicket + '€'
+            document.querySelector('.resultPrice').innerHTML = 'COME TUTTI:' + priceTicket + '€'
             console.log('prezzo normale:' + priceTicket + '€');
 
         }
-        
+
+
+        if (inputAge.value <= 17) {
+            document.querySelector('.resultTarif').innerHTML = 'DOVE STANNO I TUOI GENITORI'
+
+        }
+        else if (inputAge.value >= 65) {
+            document.querySelector('.resultTarif').innerHTML = 'TE MANCA POCO, TI DIAMO UNA MANO CO NO SCONTICINO'
+
+        }
+
+        else {
+            document.querySelector('.resultTarif').innerHTML = 'HAI LE FORZE E I SORDI; PAGAAAA'
+
+
+        }
+
+
     }
 )
 
